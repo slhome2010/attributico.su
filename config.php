2439e5968c922c383576b7190439105a3f3e6527
slhome2010 <slhome2010@mail.ru>
2023-01-09 20:29:46 +0600

    v3.2.5

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
-define ('VERSION', 'Currently v3.2.4');
-define ('VERSION_SHORT', 'v3.2.4');
-define ('LAST_VERSION', 'v3.2.4');
+define ('VERSION', 'Currently v3.2.5');
+define ('VERSION_SHORT', 'v3.2.5');
+define ('LAST_VERSION', 'v3.2.5');
